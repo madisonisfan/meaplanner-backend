1dcfda374fb4af8a555df2ae64842f94b63fc8e1
Madison Isfan <isfanmadison@gmail.com>
2021-07-07 13:30:01 -0700

    Passport Facebook(OAuth)-not tested

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/mealplanner",
+  facebook: {
+    clientId: "404240704255523",
+    clientSecret: "2024075ebd0c18a675284613ea8d4826",
+  },
 };
